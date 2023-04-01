cf733028dd12bd1d69f9ac9ece7a9493df6669d0
all-drama <90520716+all-drama@users.noreply.github.com>
2023-04-01 17:25:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-Nn
+from fastapi import FastAPI
+from fastapi.params import Query
+import yt_dlp
+
+app = FastAPI()
+
+@app.get("/download")
+async def download_video(url: str = Query(...)):
+    ydl_opts = {
+        'outtmpl': '%(title)s.%(ext)s',
+        'format': 'best',
+        'quiet': True,
+    }
+    
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        filename = ydl.prepare_filename(info)
+    
+    return {"filename": filename}
